52b967715c56b351caed657b142ecb21e360641c
MrPaoBrother <799906484@qq.com>
2019-06-22 01:13:13 +0800

    uniq longest substring

---
+++ unique_longest_substring/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "solution.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    Solution sol;
+    cout << sol.lengthOfLongestSubstring("bbb") << endl;
+    return 0;
+}
